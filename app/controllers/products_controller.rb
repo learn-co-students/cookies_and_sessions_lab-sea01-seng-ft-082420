d8335ec1cb71ee7e9ae703f6d86f89d19eb6fd47
Cooliomoded <cooliomoded@gmail.com>
2020-09-24 15:49:02 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+    end
+
+    def add
+        @product = Product.find(params[:id])
+        cart << @product.id
+        redirect_to products_path
+    end
+
+end
